80930bb7d7c1c1236f0c77dba91575b337ad529e
令狐冲 <617002850@qq.com>
2017-03-19 16:24:59 +0800

    creat2

--- src/com/anyi/test.java
+++ src/com/anyi/test.java
@@ -1,5 +1,15 @@
 package com.anyi;
 
-public class test {
+import org.junit.Test;
 
+public class test {
+	public void getSb(){
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("-");
+				}
+				System.out.println("You are SB!");
+			}
+	}
 }
+
